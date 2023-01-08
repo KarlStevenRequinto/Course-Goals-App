99c0468b7c66512c1842b3f11eb11288928a0110
KarlStevenRequinto <lerdrobott@gmail.com>
2023-01-09 02:09:24 +0800

    replacing lists with scrollview to flatlist

--- App.js
+++ App.js
@@ -1,15 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInput = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoal = () => {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, key: Math.random().toString() },
+    ]);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInput}
+        />
+        <Button title="Add Goal" onPress={addGoal} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        <FlatList
+          data={courseGoals}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.goalItemContainer}>
+                <Text style={styles.goalItem}>{item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => item.key}
+        />
+
+        {/* {courseGoals.map((goal) => (
+            <View key={goal} style={styles.goalItemContainer}>
+              <Text style={styles.goalItem}>{goal}</Text>
+            </View>
+          ))} */}
       </View>
     </View>
   );
@@ -40,5 +81,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItemContainer: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalItem: {
+    color: "white",
   },
 });
